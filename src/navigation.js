ec32ec6a5e105419b243cf704562362f75a46a84
Leo-Contreras <87204209+Leo-Contreras@users.noreply.github.com>
2023-09-12 11:15:29 -0700

    update
    aaa

--- src/navigation.js
+++ src/navigation.js
@@ -18,8 +18,12 @@ export const headerData = {
       text: 'Contacto',
       href: getPermalink('/contacto'),
     },
+    {
+      text: 'Cotización',
+      href: getPermalink('/cotizacion'),
+    },
   ],
-  actions: [{ text: 'Cotización', href: '/cotizacion', target: '_blank' }],
+  actions: [],
 };
 
 export const footerData = {
@@ -77,8 +81,6 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
